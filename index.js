4b841d69ca7fabb413680af9a4810f3701165314
PreyMa <mlpreymann@gmail.com>
2023-06-12 00:51:16 +0200

    Implement key set rotation
    Keys are now automatically changed regularly. The system keeps track of old key sets to be able to verify old tickets.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 import crypto from 'node:crypto'
 import fs from 'node:fs'
+import path from 'node:path'
 import express from 'express'
 import bodyParser from 'body-parser'
 import dotenv from 'dotenv'
@@ -13,11 +14,118 @@ import {HttpError} from 'http-errors'
 
 dotenv.config()
 
-function loadKeys() {
-  const privateKey= crypto.createPrivateKey( fs.readFileSync(process.env.PRIVATE_KEY) )
-  const publicKey= crypto.createPublicKey( fs.readFileSync(process.env.PUBLIC_KEY) )
+function createKeyPair() {
+  console.log('Creating new key pair...')
 
-  return {privateKey, publicKey}
+  // Create new key pair
+  const keys= crypto.generateKeyPairSync('rsa', { modulusLength: 2048 })
+
+  // Create timestamp and unique id
+  const id= uuid.v4()
+  const time= Date.now()
+  const hexTime= time.toString(16)
+
+  // Write the keys to disk
+  const publicKeyPath= path.join( process.env.KEYS_DIR, `${hexTime}.${id}.public.pem` )
+  const publicKeyPem= keys.publicKey.export({type: 'pkcs1', format: 'pem'})
+  fs.writeFileSync( publicKeyPath, publicKeyPem )
+
+  const privateKeyPath= path.join( process.env.KEYS_DIR, `${hexTime}.${id}.private.pem` )
+  fs.writeFileSync( privateKeyPath, keys.privateKey.export({type: 'pkcs1', format: 'pem'}) )
+
+  return {
+    uuid: id,
+    time,
+    private: keys.privateKey,
+    public: keys.publicKey,
+    publicKeyPem
+  }
+}
+
+const keyPairMaxAge= parseInt(process.env.KEY_PAIR_MAX_AGE)* 24* 60* 60* 1000
+function loadKeys() {
+  const keysMap= new Map()
+
+  // Go through the directory of keys and take each file that has a filename
+  // matching the required schema. Each file is added to a map of key pairs.
+  const keysDirectory= fs.opendirSync(process.env.KEYS_DIR)
+  let dirEntry= null
+  try {
+    while((dirEntry = keysDirectory.readSync()) !== null) {
+      // Try to match the file name to the schema
+      const nameSplitter= /^(?<time>[0-9a-fA-F]+)\.(?<uuid>[0-9a-fA-F]{8}[-:][0-9a-fA-F]{4}[-:][0-9a-fA-F]{4}[-:][0-9a-fA-F]{4}[-:][0-9a-fA-F]{12})\.(?<type>private|public)\.pem$/
+      const match= dirEntry.name.match(nameSplitter)
+      if( match ) {
+        // Extract all information from the file name string
+        const uuid= match.groups.uuid
+        const time= parseInt(match.groups.time, 16)
+        const type= match.groups.type
+  
+        // Get or create map entry
+        let mapEntry= keysMap.get(uuid)
+        if(!mapEntry) {
+          mapEntry= {uuid, time}
+          keysMap.set(uuid, mapEntry)
+        }
+  
+        if(mapEntry.time !== time) {
+          throw Error(`Key files '${uuid}' have mismatching times`)
+        }
+  
+        if( mapEntry.hasOwnProperty(type) ) {
+          throw Error(`Key files '${uuid}' have multiple files for each key file type`)
+        }
+  
+        // Add the file path to the entry by key file type
+        mapEntry[type]= path.join(process.env.KEYS_DIR, dirEntry.name)
+      }
+    }
+  } finally {
+    keysDirectory.close()
+  }
+
+  // Iterate over all entries to find the newest by its time. Also check
+  // if all entries have both key file types
+  let newestKeyPair= null
+  for(const [uuid, entry] of keysMap) {
+    if( !entry.private || !entry.public ) {
+      throw Error(`Key files '${uuid} is missing a key file type (private or public)'`)
+    }
+
+    if( newestKeyPair === null || newestKeyPair.time < entry.time) {
+      newestKeyPair= entry
+    }
+  }
+
+  // Create a new key pair if the newest key pair is too old
+  if( Date.now() - newestKeyPair.time > keyPairMaxAge) {
+    const newKeyEntry= createKeyPair()
+    keysMap.set(newKeyEntry.uuid, newKeyEntry)
+    newestKeyPair= newKeyEntry
+  }
+
+  // Load the public keys for all entries and remove the private keys. Only
+  // the newest entry loads its private key
+  for(const [uuid, entry] of keysMap) {
+    // Skip the entry if it already has its keys loaded
+    if(typeof entry.public !== 'string' && typeof entry.private !== 'string') {
+      continue
+    }
+
+    entry.publicKeyPem= fs.readFileSync(entry.public, 'utf8')
+    entry.public= crypto.createPublicKey( entry.publicKeyPem )
+
+    if(entry === newestKeyPair) {
+      entry.private= crypto.createPrivateKey( fs.readFileSync(entry.private) )
+    } else {
+      entry.private= null
+    }
+  }
+
+  // console.log(keysMap)
+  console.log(`Loaded ${keysMap.size} keys. Current key pair is '${newestKeyPair.uuid}'`)
+
+  return { keysMap, newestKeyPair }
 }
 
 function loadPages() {
@@ -34,6 +142,7 @@ function createTicket(customData) {
     issuer: process.env.ISSUER_ID,
     timeStamp: new Date().toISOString(),
     type: 'v1',
+    cert: currentKeyPair.uuid,
     customData
   }
 
@@ -41,7 +150,7 @@ function createTicket(customData) {
   sign.write(JSON.stringify(content))
   sign.end()
 
-  const signature = sign.sign(privateKey, 'base64')
+  const signature = sign.sign(currentKeyPair.private, 'base64')
   return { content, signature }
 }
 
@@ -55,12 +164,14 @@ const ticketSchema= joi.object({
     issuer: joi.string().equal(process.env.ISSUER_ID).required(),
     timeStamp: joi.string().isoDate().required(),
     type: joi.string().equal('v1').required(),
+    cert: joi.string().uuid({version: 'uuidv4'}).required(),
     customData: joi.object().unknown(true).required()
   }).required(),
   signature: joi.string().base64().length(344).required()
 })
 
 function verifyTicket(ticket) {
+  // Validate schema
   const result= ticketSchema.validate(ticket, validationOptions)
   if( result.error ) {
     let message= 'unknown error'
@@ -70,19 +181,41 @@ function verifyTicket(ticket) {
     return { error: `Schema error: ${message}`}
   }
 
+  // Get key pair entry
+  const keyPair= keysMap.get(ticket.content.cert)
+  if(!keyPair) {
+    return { error: 'Unknown cert id' }
+  }
+
+  // Verify signature
   const verify = crypto.createVerify('SHA256')
   verify.write(JSON.stringify(ticket.content))
   verify.end()
-  const isValid= verify.verify(publicKey, ticket.signature, 'base64')
+  const isValid= verify.verify(keyPair.public, ticket.signature, 'base64')
   return {error: isValid ? null : 'Signature error'}
 }
 
-// Load the keys and pages, and create the express app
+// Load the keys and pages
 const maxNumCustomDataBytes= parseInt(process.env.CUSTOM_DATA_LIMIT);
-const {privateKey, publicKey}= loadKeys()
+const {keysMap, newestKeyPair}= loadKeys()
 const {homePage, error404Page, error500Page}= loadPages()
-const app= express()
 
+// Add handler to automatically update the keys
+let currentKeyPair= newestKeyPair;
+setInterval(() => {
+  // Check age every 10 minutes
+  if(Date.now() -currentKeyPair.time > keyPairMaxAge ) {
+    const newKeyEntry= createKeyPair()
+    keysMap.set(newKeyEntry.uuid, newKeyEntry)
+
+    // Release the private key of the old key entry
+    currentKeyPair.private= null
+    currentKeyPair= newKeyEntry
+  }
+}, 10* 60* 1000)
+
+// Create the express app
+const app= express()
 app.use(express.static('public'))
 app.use(helmet())
 app.use(cors())
@@ -93,12 +226,20 @@ app.get('/', (req, res) => {
   res.send(homePage)
 })
 
-// API -> /cert returns the public key as PEM file string
-const publicKeyPemString= publicKey.export({type: 'pkcs1', format: 'pem'})
-app.get('/v1/api/cert', (req, res) => {
+// API -> /cert returns the requested public key as PEM file string
+app.get('/v1/api/cert/:uuid?', (req, res) => {
+  const keyPair= req.params.uuid ? keysMap.get(req.params.uuid) : currentKeyPair
+  if(!keyPair) {
+    res
+    .type('text/plain')
+    .status(404)
+    .send('Unknown key id')  
+    return
+  }
+
   res
     .type('text/plain')
-    .send(publicKeyPemString)
+    .send(keyPair.publicKeyPem)
 })
 
 // API -> /ticket returns a new timestamp ticket as JSON
